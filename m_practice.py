bdab1c6aa7f95f7b7b8ae5ce10627899f390d58b
Eric Luc <luckhoi89@gmail.com>
2021-08-04 11:45:42 -0700

    done

--- m_practice.py
+++ m_practice.py
@@ -1,38 +1,40 @@
 # >>> x = False
 # >>> y = True
 # >>> x
-# print:False
+# False
 # >>> y
-# ???
+# True
 # >>> type(x)
-# ???
+# boolean
 # >>> type(y)
-# ???
+# bool
 # >>> x or y
-# ???
+# True
 # >>> x and y
-# ???
+# False
 # >>> not x and y
-# ???
+# True
 # >>> x == True
-# ???
+# False
 # >>> x == 0
-# ???
+# True
 # >>> x == ""
-# ???
+# False
 # >>> x or 1/0
-# ???
+# 
 # >>> x and 1/0
-# ???
+# False
 # >>> y or 1/0
-# ???
+# True
 # # >>> y and 1/0
-# ???
+# False
 # >>> id(x)
 # ???
 # >>> w = x
 # >>> z = True
 # >>> id(w) == id(x)
-# ???
+# True
 # >>> id(z) == id(y)
-# ???
+# False
+
+SCHOOLED
--- sequence_type_exercise.py
+++ sequence_type_exercise.py
@@ -1,26 +1,10 @@
-d = {"one": 1, "two": 2, "three": 3}
-print(d["two"])
-#2
-d["three"] = 5
-## d = {"one": 1, "two": 2, "three": 5}
-del d["one"]
-print(len(d))
-#2
-d["four"] = 11
-## d = {"one": 1, "two": 2, "three": 5,"four":8}
-# print(d.get("three", "four"))
-print(d)
-#5
-# print(d.get("three", d["four"]))
-#5
-# print(max(d.values()))
-# #8
-# print(min(d))
-# #four
-# print(d.pop("four", 4))
-# #8
-# print(d.pop("one", d["two"]))
-# #
-# print(d.pop("two"))
-# for key, value in d.items():
-#     print(key * value)
+"""Check whether a word is 'efficient' - if each letter can be drawn by a pencil without lifting."""
+EFFICIENT_LETTERS = set('BCDGIJLMNOPSUVWZ')
+
+def is_efficient(letters):
+    if(letters - EFFICIENT_LETTERS == set()):
+        return True
+    return False
+
+print(is_efficient(set("BCD")))
+print(set("BCDK")-EFFICIENT_LETTERS)
