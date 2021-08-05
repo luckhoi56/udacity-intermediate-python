61d69cb83d879470c642f48bf401ae527e1bbc00
Eric Luc <luckhoi89@gmail.com>
2021-08-04 23:13:01 -0700

    newly commit

--- sequence_type_exercise.py
+++ sequence_type_exercise.py
@@ -1,12 +1,22 @@
-"""Swap the keys and values in a mapping."""
+from wordsets import english_words, english_words_small
 
-def swap_keys_and_values(d):
-    for key,value in d.items():
-        print(key,value)
-        key,value=value,key
-        print(key,value)
-    return d
 
-a={"a":"leaf","b":"mouse"}
-a = swap_keys_and_values(a)
-print(a)
+def find_anagrams(letters, words):
+    """Find a collection of anagrams of given letters from a given word bank.
+
+    :param letters: The letters from which to form anagrams.
+    :param words: A set of lowercase, alphabetic English words in a word bank.
+    :return: A set of anagrams of the given letters found in the word bank.
+    """
+    #### ADD YOUR CODE BELOW ####
+
+
+    #### ADD YOUR CODE ABOVE ####
+
+
+if __name__ == '__main__':
+    while True:
+        letters = input("What letters would you like to find the anagram of? ").lower().strip()
+        print(letters)
+        for anagram in find_anagrams(letters, english_words_small):
+            print(anagram)
---
+++ wordsets.py
@@ -0,0 +1,2 @@
+letters = input("What letters would you like to find the anagram of? ").lower().strip()
+print(letters)
