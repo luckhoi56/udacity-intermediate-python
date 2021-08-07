068b7b7f0cf2066a67abe359ec2ec1d648e167b4
Eric Luc <luckhoi89@gmail.com>
2021-08-06 21:03:04 -0700

    make some comments

--- t9.py
+++ t9.py
@@ -17,23 +17,23 @@ if __name__ == '__main__':
     # When you've finished implementing a part, remove the `gold.` prefix to check your own code.
 
     # PART 1: Parsing a string into a dictionary.
-    words = gold.parse_content(content)
-
+    #words = gold.parse_content(content)
+    print(words)
     # PART 2: Building a trie from a collection of words.
-    tree = gold.make_tree(words)
+    # tree = gold.make_tree(words)
 
-    while True:
-        # PART 3: Predict words that could follow
-        numbers = helper.ask_for_numbers()
-        predictions = gold.predict(tree, numbers)
+    # while True:
+    #     # PART 3: Predict words that could follow
+    #     numbers = helper.ask_for_numbers()
+    #     predictions = gold.predict(tree, numbers)
 
-        if not predictions:
-            print('No words were found that match those numbers. :(')
-        else:
-            for prediction, frequency in predictions[:10]:
-                print(prediction, frequency)
+    #     if not predictions:
+    #         print('No words were found that match those numbers. :(')
+    #     else:
+    #         for prediction, frequency in predictions[:10]:
+    #             print(prediction, frequency)
 
-        response = input('Want to go again? [y/N] ')
-        again = response and response[0] in ('y', 'Y')
-        if not again:
-            break
+    #     response = input('Want to go again? [y/N] ')
+    #     again = response and response[0] in ('y', 'Y')
+    #     if not again:
+    #         break
