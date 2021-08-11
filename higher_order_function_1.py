49be95d71f23d99cc46cac736723ffc58a7c69e8
Eric Luc <luckhoi89@gmail.com>
2021-08-10 23:39:52 -0700

    done the first one

--- higher_order_function_1.py
+++ higher_order_function_1.py
@@ -22,3 +22,19 @@ print(tuple(a))
 print(tuple(b))
 print(tuple(c))
 print(tuple(d))
+
+
+
+# Practice with filter
+# Fill out the rest of the filter functions.
+# You can define additional functions if you need to.
+# (a) range(100) => (0, 3, 6, 9, ...)  (div by 3)
+# (b) range(100) => (0, 5, 10, 15, ...)  (div by 5)
+# (c) range(100) => (0, 15, 30, 45, ...)  (div by 15)
+# (d) range(100) => (1, 2, 4, 7, 8, 11, 13, 14, 16, 17, ...)  (not div by 3 and not div by 5)
+
+a = filter(lambda val: val % 3 == 0, range(100))
+# b = filter(???, ???)
+# c = filter(???, ???)
+# d = filter(???, ???)
+print(tuple(a))
