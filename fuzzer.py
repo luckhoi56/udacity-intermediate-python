7465411b21f3e127d3d3c3063a7423c71e33c088
Eric Luc <luckhoi89@gmail.com>
2021-08-11 09:59:21 -0700

    working now

--- fuzzer.py
+++ fuzzer.py
@@ -1,12 +1,19 @@
 """Generate an infinite stream of successively larger random lists."""
 import helper_2
-def generate_cases(size):
+def generate_cases():
+    count = 1
     while True:
-        yield helper_2.random_list(size)
+        
+        yield helper_2.random_list(count)
+        count = count + 1
     
         
-if __name__ == '__main__':
-    for case in generate_cases(10):
-        if len(case) > 10:
-            break
-        print(case)
+# if __name__ == '__main__':
+#     for case in generate_cases():
+#         if len(case) > 10:
+#             break
+#         print(case)
+
+g = generate_cases()
+print(next(g))
+print(next(g))
